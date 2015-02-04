f18f198637765f330a8e16a6fc0d8a059caf6aae
limsim <lim.sim@gmail.com>
2015-02-04 23:25:23 +0000

    updated test to run against production

--- meetup-cd-basic/src/test/java/basicTest.java
+++ meetup-cd-basic/src/test/java/basicTest.java
@@ -10,6 +10,8 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class basicTest {
 
@@ -20,15 +22,20 @@ public class basicTest {
     private static final String USERNAME = "limsim1";
     private static final String AUTOMATE_KEY = "yU8WsQkyFgysDpuReGP3";
     private static final String URL = "http://" + USERNAME + ":" + AUTOMATE_KEY + "@hub.browserstack.com/wd/hub";
+    private static final Map<String, String> envHost = new HashMap<String, String>();
 
     @BeforeSuite
     public void openBrowser() throws Exception {
+        envHost.put("local", "https://s3-eu-west-1.amazonaws.com/cd-meetup-dev");
+        envHost.put("dev", "https://s3-eu-west-1.amazonaws.com/cd-meetup-dev");
+        envHost.put("prod", "https://s3-eu-west-1.amazonaws.com/cd-meetup-prod");
+
         if (environment.equals("local")) {
             seleniumServer = new SeleniumServer();
             seleniumServer.start();
 
             browser = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.firefox());
-        } else if (environment.equals("dev")) {
+        } else if (environment.equals("dev") || environment.equals("prod")) {
             DesiredCapabilities caps = new DesiredCapabilities();
             caps.setCapability("browser", "Firefox");
             caps.setCapability("browser_version", "35.0");
@@ -46,14 +53,17 @@ public class basicTest {
         if (environment.equals("local")) {
             browser.quit();
             seleniumServer.stop();
-        } else if (environment.equals("dev")) {
+        } else if (environment.equals("dev") || environment.equals("prod")) {
             browser.quit();
         }
     }
 
     @Test
     public void checkMessage() {
-        browser.get("https://s3-eu-west-1.amazonaws.com/cd-meetup-dev/index.html");
+        String testUrl = envHost.get(environment) + "/index.html";
+        System.out.println("testUrl: " + testUrl);
+        
+        browser.get(testUrl);
         WebElement message = browser.findElement(By.id("tag1"));
         Assert.assertEquals(message.getText(), "my first page 5");
     }
